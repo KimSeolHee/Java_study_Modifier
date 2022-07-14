4608b5d0ace386656b8c73e3fb54c3aa39c3df53
KimSeolHee <tjfgml64@nate.com>
2022-07-14 16:22:09 +0900

    getter setter

---
+++ src/com/iu/animal/Cat.java
@@ -0,0 +1,15 @@
+package com.iu.animal;
+
+public class Cat {
+	
+	private int age;
+	
+	void sound() {
+		System.out.println("야옹~");
+	}
+	
+	private Cat() {
+		
+	}
+
+}
---
+++ src/com/iu/main/Main.java
@@ -0,0 +1,23 @@
+package com.iu.main;
+
+import com.iu.animal.Cat;
+import com.iu.animal.Human;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Human human = new Human();
+//		human.age = 560;
+		human.setAge(160);
+		human.info();
+		
+		
+//		Cat cat = new Cat();
+//		cat.sound();
+//		cat.age(5);
+		
+		
+	}
+
+}
